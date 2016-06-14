3cd4000655f7517fd617586a56befeeb23da4bc9
Timbm3 <timbrinkmadsen@live.se>
2016-06-14 22:19:03 +0200

    Lab

---
+++ MVCLab2TicTacToe/Controllers/TicTacToeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLab2TicTacToe.Models;
+
+namespace MVCLab2TicTacToe.Controllers
+{
+    public class TicTacToeController : Controller
+    {
+        // GET: TicTacToe
+        public ActionResult Index()
+        {
+            TicTacToeModel m = new TicTacToeModel();
+            return View(m);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TicTacToeModel model)
+        {
+            if (!model.GameOver)
+            {
+                model.AddNumber();
+                model.CheckIfWon();
+                ModelState.Clear();
+            }
+            return View(model);
+        }
+    }
+}
